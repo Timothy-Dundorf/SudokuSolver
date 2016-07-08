20208fdb8c8e9676b55397cd80549b49dde2c121
t1mothyjam3s <dundorft@vt.edu>
2016-07-08 16:56:20 -0400

    Complex solution started. Not everything works yet.

--- solver.js
+++ solver.js
@@ -25,8 +25,9 @@ var sudoku = (function () {
         var currentCell = getCell(rowNumber, columnNumber);
 
         if (currentCell !== null){
-            removeFromBoard(rowNumber,columnNumber, currentCell);
-            candidateBoard[rowNumber][columnNumber] = [-1 * currentCell]; 
+            remove(rowNumber,columnNumber, currentCell);
+            candidateBoard[rowNumber][columnNumber] = [-1 * currentCell];
+            handleEliminationRules(); 
             setSolos();
             
         }
@@ -43,10 +44,7 @@ var sudoku = (function () {
             rowNumber = 0; 
         }
 
-
-       
-
-        function removeFromBoard(row, column, numberToRemove){
+        function remove(row, column, numberToRemove){
             removeFromRow(row, numberToRemove);
             removeFromColumn(column, numberToRemove);
             removeFromGrid(row, column, numberToRemove);
@@ -108,7 +106,7 @@ var sudoku = (function () {
                 var soloY = firstSolo[1];
                 var soloValue = firstSolo[2];
 
-                removeFromBoard(soloX, soloY, soloValue);
+                remove(soloX, soloY, soloValue);
                 candidateBoard[soloX][soloY] = [soloValue * -1];
 
                 if(getCell(soloX, soloY) === null)
@@ -182,10 +180,10 @@ var sudoku = (function () {
 
             for (var i = 0; i < 3; i++){
                 for (var j = 0; j < 3; j++){
-                    var row = i + firstCellOfGridCoords[0];
-                    var column = j + firstCellOfGridCoords[1];
+                    var curRow = i + firstCellOfGridCoords[0];
+                    var curColumn = j + firstCellOfGridCoords[1];
 
-                    if (-1 != candidateBoard[row][column].indexOf(number))
+                    if (-1 != candidateBoard[curRow][curColumn].indexOf(number))
                         instances++;
 
                     if (instances > 1)
@@ -196,34 +194,151 @@ var sudoku = (function () {
             return true;
         }
 
-        function findSetsRow(row, setSize){
+        function handleEliminationRules(){
+            handleEliminationRulesFrom(findCandidateCellSetRow, removeFromRow);
+            handleEliminationRulesFrom(findCandidateCellSetColumn, removeFromColumn);
+        }
+
+        function handleEliminationRulesFrom(finderFunction, removalFunction){
+            for (var i = 0; i < 9; i++){
+                var tripleSets = finderFunction(i, 3);
+                var matchingTripleSubsets = findMatchingTripleSubsetsFromSet(tripleSets);
+                removeSetsFrom(matchingTripleSubsets, removalFunction, i);
+                var doubleSets = reduceCandidateSetsToDoubles(tripleSets);
+                var matchingDoubles = findMatchingDoublesFromSet(doubleSets);
+                removeSetsFrom(matchingDoubles, removalFunction, i);
+                
+                
+            }
+        }
+
+        function removeSetsFrom(setsToRemove, removalFunction, removalIndex)
+        {
+            setsToRemove.forEach(function(candidateCellSet){
+                    var numbersToRemoveArray = numbersToRemove(candidateCellSet);
+
+                    numbersToRemoveArray.forEach(function(number){
+                        removalFunction(removalIndex, number);
+                    });
+
+                    repopulateCandidateBoardFromSet(candidateCellSet);
+                });
+        }
+
+        function findMatchingTripleSubsetsFromSet(candidateCellSet){
+            var tripleSubsetArray = [];
+            var potentialCandidateCell;
+
+            while (candidateCellSet.length > 0){
+                var currentCandidateCell = candidateCellSet.pop();
+                
+                if (currentCandidateCell.cell.length <= 3)
+                {
+                    candidateCellSet.forEach(function(checkedCandidateCell){
+
+                        if(candidateCellArraysSubset(currentCandidateCell,checkedCandidateCell)){
+                            if(potentialCandidateCell !== undefined && candidateCellArraysSubset(currentCandidateCell, potentialCandidateCell))
+                                tripleSubsetArray.push([currentCandidateCell, potentialCandidateCell, checkedCandidateCell]);
+                            else 
+                                potentialCandidateCell = checkedCandidateCell;
+                        }
+                    });
+                }
+            }
+
+            return tripleSubsetArray;
+        }
+
+        function candidateCellArraysSubset(candidateCell1, candidateCell2){
+            if (candidateCellArraysEqual(candidateCell1, candidateCell2))
+                return true;
+
+            if (candidateCell1.cell.length == candidateCell2.cell.length){
+                if (candidateCell1.cell.length == 3)
+                    return false;
+                else{
+                    //TODO write the rest of this method.
+                }
+
+            }
+        }
+
+
+        function findMatchingDoublesFromSet(candidateCellSet){
+            var doublesArray = [];
+
+            while (candidateCellSet.length > 0){
+                currentCandidateCell = candidateCellSet.pop();
+                
+                candidateCellSet.forEach(function(checkedCandidateCell){
+                    
+                    if (candidateCellArraysEqual(currentCandidateCell, checkedCandidateCell))
+                        doublesArray.push([currentCandidateCell, checkedCandidateCell]);
+                });
+            }
+
+            return doublesArray; 
+        }
+
+        function candidateCellArraysEqual(candidateCell1, candidateCell2){
+            if (candidateCell1.cell.length != candidateCell2.cell.length)
+                return false;
+
+            for (var i = 0; i < candidateCell1.cell.length; i++){
+                if(candidateCell1.cell[i] != candidateCell2.cell[i])
+                    return false
+            }
+
+            return true;
+        }   
+
+        function numbersToRemove(candidateCellSet){
+            var numbersArray = [];
+
+            candidateCellSet.forEach(function(candidateCell){
+                candidateCell.cell.forEach(function(number){
+                    if (-1 == numbersArray.indexOf(number))
+                        numbersArray.push(number);
+                });
+            });
+
+            return numbersArray;
+        }
+
+        function repopulateCandidateBoardFromSet(candidateCellSet){
+            candidateCellSet.forEach(function(candidateCell){
+                candidateBoard[candidateCell.row][candidateCell.column] = candidateCell.cell;
+            });
+        }
+
+        function findCandidateCellSetRow(row, setSize){
             var sets = []; 
 
             for (var column = 0; column < 9; column++){
                 var candidateCell = candidateBoard[row][column];
 
-                if (setSize <= candidateCell.length)
-                    sets.push(candidateCell);
+                if (setSize >= candidateCell.length && 1 < candidateCell.length)
+                    sets.push({row:row, column:column, cell:candidateCell});
             }
 
             return sets; 
 
         }
 
-        function findSetsColumn(column, setSize){
+        function findCandidateCellSetColumn(column, setSize){
             var sets = [];
 
             for (var row = 0; row < 9; row++){
                 var candidateCell = candidateBoard[row][column];
 
-                if (setSize <= candidateCell.length)
-                    sets.push(candidateCell);
+                if (setSize >= candidateCell.length && 1 < candidateCell.length)
+                    sets.push({row:row, column:column, cell:candidateCell});
             }
 
             return sets;
         }
 
-        function findSetsGrid(row, column, setSize){
+        function findCandidateCellSetGrid(row, column, setSize){
             var sets = [];
             var firstCellOfGridCoords = firstCellOfGridCoordinates(row, column);
 
@@ -233,11 +348,17 @@ var sudoku = (function () {
                     var checkedColumn = j + firstCellOfGridCoords[1];
                     var candidateCell = candidateBoard[checkedRow][checkedColumn];
 
-                     if (setSize <= candidateCell.length)
-                    sets.push(candidateCell);
+                     if (setSize >= candidateCell.length)
+                    sets.push({row:row, column:column, cell:candidateCell});
                 }
-
+            }
             return sets;
+        }
+
+        function reduceCandidateSetsToDoubles(candidateCellSet){
+            return candidateCellSet.filter(function(candidateCell){
+                return candidateCell.cell.length <= 2; 
+            });
         }
     };
 
